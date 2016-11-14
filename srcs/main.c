5a5b3ed4b730790faf6a77d018593145f34532a0
Snosky <tpayen@student.42.fr>
2016-11-14 02:13:16 +0100

    Some test

--- srcs/main.c
+++ srcs/main.c
@@ -24,15 +24,22 @@ void	sig_win_resize(int i)
 			printf("Error 4\n"); // TODO : Error
 }
 
+void	sig_exit(int i)
+{
+	t_term	*term;
+	
+	term = ft_term();
+	(void)i;
+
+	tputs(tgetstr("te", NULL), 0, tputc);
+	tcsetattr(0, TCSADRAIN, &(term->default_term));
+	exit(EXIT_SUCCESS);
+}
+
 int main	(int ac, char **av)
 {
-		(void)ac;
-		(void)av;
 	t_term	*term = ft_term();
 	int		success;
-
-	//struct termios	default_term;
-	//struct termios	term;
 
 	// On recup les informations obligatoire du terminal
 	term->name = getenv("TERM");
@@ -46,8 +53,6 @@ int main	(int ac, char **av)
 		return (printf("Error 2\n")); // TODO : Error : "Terminal type $termtype is not defined."
 
 	
-	// On passe en mode pleine ecran
-	tputs(tgetstr("ti", NULL), 0, tputc);
 	
 	
 	if (tcgetattr(0, &(term->term)) == -1 || tcgetattr(0, &(term->default_term)))
@@ -58,24 +63,27 @@ int main	(int ac, char **av)
 	term->term.c_cc[VTIME] = 0;
 
 	tcsetattr(0, TCSADRAIN, &(term->term));
-
-	// Active arrow key
-	tputs(tgetstr("ks", NULL), 0, tputc);
-//	tputs(tgetstr("ku", NULL), 0, tputc);
-
-	char key[3];
+	
+	// On passe en mode pleine ecran
+	tputs(tgetstr("ti", NULL), 0, tputc);
+	//On cache le cursor
+	tputs(tgetstr("vi", NULL), 0, tputc);
 
 	sig_win_resize(0); // Call car initialization
+	signal(SIGWINCH, sig_win_resize);
+	signal(SIGINT, sig_exit); // Signal Ctrl-C
+	
+	char key[3];
+
 	while (1)
 	{
 		// On clear le terminal
 		tputs(tgetstr("cl", NULL), 0, tputc);
 
 		//Window resize : TODO lib ncurses ?!
-		signal(SIGWINCH, sig_win_resize);
 
 		//Test center
-		char *middle = "Hello World";
+		char *middle = "Hello World\n";
 		int left_col = (term->winsize.ws_col / 2) - (ft_strlen(middle) / 2);
 		for (int s = 0; s < left_col; s++)
 			tputs(" ", 0, tputc);
@@ -83,29 +91,24 @@ int main	(int ac, char **av)
 		tputs(tgetstr("us", NULL), 0, tputc); // Active underline
 		tputs(middle, 0, tputc);
 		tputs(tgetstr("me", NULL), 0, tputc); // Remove all text style (ue for only underline);
-		tputs(ft_itoa(term->winsize.ws_col), 0, tputc);
 		//End test
-
-
-		ft_bzero(key, 3);
-		read(0, key, 3);
+	
+		for (int k = 0; k < ac; k++)
+			tputs(av[k], 0, tputc); tputs("\n", 0, tputc);
 
 		// Arrow test
 		if (ft_strcmp(key, tgetstr("kl", NULL)) == 0)
-			tputs("Left Arrow", 0, tputc);
+			tputs("Left Arrow\n", 0, tputc);
+		else if (ft_strcmp(key, tgetstr("ku", NULL)) == 0)
+			tputs("Up Arrow\n", 0, tputc);
+		else if (ft_strcmp(key, tgetstr("kd", NULL)) == 0)
+			tputs("Down Arrow\n", 0, tputc);
+		else if (ft_strcmp(key, tgetstr("kr", NULL)) == 0)
+			tputs("Right Arrow\n", 0, tputc);
 		// End test
-
-		/*if (key[0] == 27)
-		{
-			if (key[1] == 91)
-				tputs("Arrow\n", 0, tputc); // TODO : Passe pas une fois sur deux
-			else if (key[1] == 0)
-			{
-				tputs(tgetstr("te", NULL), 0, tputc); 
-				tcsetattr(0, TCSADRAIN, &(term->default_term));
-				return (0);
-			}
-		}*/
+		
+		ft_bzero(key, 3);
+		read(0, key, 3);
 	}
 
 
