532103c174947d6bdf115f9d6d207996819788eb
Snosky <tpayen@student.42.fr>
2016-11-16 15:10:11 +0100

    Add case where there is a status line

--- srcs/print_entries.c
+++ srcs/print_entries.c
@@ -6,46 +6,53 @@
 /*   By: tpayen <tpayen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 01:00:24 by tpayen            #+#    #+#             */
-/*   Updated: 2016/11/16 02:33:21 by tpayen           ###   ########.fr       */
+/*   Updated: 2016/11/16 15:06:03 by tpayen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_select.h>
 
+static void	print_entry(int i, t_term *term, int *colcount, int *row, int nbrow)
+{
+	int	col;
+
+	if (term->entries[i].visible == 0)
+		return ;
+	if (*row > nbrow)
+	{
+		*row = 0;
+		(*colcount)++;
+	}
+	col = *colcount * (term->longest + 2);
+	tputs(tgoto(term->cap[CM], col, *row), 1, tputc);
+	if (term->hover == i)
+		tputs(term->cap[US], 0, tputc);
+	if (term->entries[i].selected)
+		tputs(term->cap[MR], 0, tputc);
+	tputs(term->entries[i].name, 0, tputc);
+	tputs(term->cap[ME], 0, tputc);
+	(*row)++;
+}
+
 void	print_entries(void)
 {
 	t_term	*term;
-	int		col;
 	int		colcount;
 	int		row;
 	int		i;
+	int		nbrow;
 
 	term = ft_term();
 	colcount = 0;
 	row = 0;
 	i = 0;
+	nbrow = term->winsize.ws_row;
+	if (tgetflag("hs"))
+		nbrow = term->winsize.ws_row - tgetnum("ws");
 	while (i < term->nb_entries)
 	{
-		if (term->entries[i].visible == 0)
-		{
-			i++;
-			continue;
-		}
-
-		if (row > term->winsize.ws_row)
-		{
-			row = 0;
-			colcount++;
-		}
-		col = colcount * (term->longest + 2);
-		tputs(tgoto(term->cap[CM], col, row), 1, tputc);
-		if (term->hover == i)
-			tputs(term->cap[US], 0, tputc);
-		if (term->entries[i].selected)
-			tputs(term->cap[MR], 0, tputc);
-		tputs(term->entries[i].name, 0, tputc);
-		tputs(term->cap[ME], 0, tputc);
-		row++;
+		print_entry(i, term, &colcount, &row, nbrow);
 		i++;
 	}
+	ft_putnbr(tgetflag("hs"));
 }
